d65e9e3d4e9a12ac44b85d43ad7b2990fabcf5e1
richhcirerror <rich.hcir.error@gmail.com>
2019-08-22 23:48:15 +0400

    создание счетчиков

--- app/src/main/java/ru/startandroid/sendmeters/SendEmailActivity.java
+++ app/src/main/java/ru/startandroid/sendmeters/SendEmailActivity.java
@@ -1,65 +1,70 @@
 package ru.startandroid.sendmeters;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ViewFlipper;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
-public class SendEmailActivity extends AppCompatActivity implements View.OnClickListener {
+import ru.startandroid.sendmeters.ru.startandroid.sendmeters.datapicker.DataPicker;
+
+public class SendEmailActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
     private EditText etKithenColdWaterMeter;
     private EditText etKithenHotWaterMeter;
     private EditText etBathroomColdWaterMeter;
     private EditText etBathroomHotWaterMeter;
     private EditText etElectricityMeter;
-    private Button btnCreateEmail;
+    private Button btnAddCounter;
+
+    private List<ConstraintLayout> counters;
+    private float fromPosition;
+    private ViewFlipper flipper;
+    private LayoutInflater inflater;
+
     String[] monthNames = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
-    private String text = "<div>\n" +
-            "Здравствуйте</div>\n" +
-            "<div>\n" +
-            "показания счетчиков за <strong>июль</strong>.</div>\n" +
-            "<div>\n" +
-            "Адрес: <strong>ул .4 - я линия, д .22 / 24</strong>.</div>\n" +
-            "<div>\n" +
-            "Квартира: <strong>101</strong>.</div>\n" +
-            "<div>\n" +
-            "Собственник: <strong>Полулях Д.М</strong>.</div>\n" +
-            "<div>\n" +
-            "ХВС 1(2710267) - 121,</div>\n" +
-            "<div>\n" +
-            "ХВС 2(28472685) - 271,</div>\n" +
-            "<div>\n" +
-            "ГВС 3(2634870) - 78,</div>\n" +
-            "<div>\n" +
-            "ГВС 4(28472811) - 148,</div>\n" +
-            "<div>\n" +
-            "Электросчетчик - 3823</div>";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnCreateEmail = (Button) findViewById(R.id.btnCreateEmail);
-        btnCreateEmail.setOnClickListener(this);
+        btnAddCounter = (Button) findViewById(R.id.btnAddCounter);
+        btnAddCounter.setOnClickListener(this);
+        counters = new ArrayList<>();
+        // Устанавливаем listener касаний, для последующего перехвата жестов
+        ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.mainLayout);
+        mainLayout.setOnTouchListener(this);
         getSupportActionBar().hide();
-        etKithenColdWaterMeter = (EditText) findViewById(R.id.etKithenColdWaterMeter);
-        etKithenHotWaterMeter = (EditText) findViewById(R.id.etKithenHotWaterMeter);
-        etBathroomColdWaterMeter = (EditText) findViewById(R.id.etBathroomColdWaterMeter);
-        etBathroomHotWaterMeter = (EditText) findViewById(R.id.etBathroomHotWaterMeter);
-        etElectricityMeter = (EditText) findViewById(R.id.etElectricityMeter);
+        // Получаем объект ViewFlipper
+        flipper = (ViewFlipper) findViewById(R.id.flipper);
 
+        // Создаем View и добавляем их в уже готовый flipper
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //flipper.addView(createCounter());
+        //flipper.addView(createCounter());
+        //flipper.showNext();
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnCreateEmail:
-                createEmail();
+            case R.id.btnAddCounter:
+                ConstraintLayout counter = createCounter();
+                counters.add(counter);
+                flipper.addView(counter);
 
         }
     }
@@ -110,4 +115,110 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
     }
 
 
+    @Override
+    public boolean onTouch(View v, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN: // Пользователь нажал на экран, т.е. начало движения
+                // fromPosition - координата по оси X начала выполнения операции
+                fromPosition = motionEvent.getX();
+                break;
+            case MotionEvent.ACTION_UP: // Пользователь отпустил экран, т.е. окончание движения
+                float toPosition = motionEvent.getX();
+                if (fromPosition > toPosition) {
+                    flipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.go_next_in));
+                    flipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.go_next_out));
+                    flipper.showNext();
+                } else if (fromPosition < toPosition) {
+                    flipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.go_prev_in));
+                    flipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.go_prev_out));
+                    flipper.showPrevious();
+                    flipper.getCurrentView()
+                }
+            default:
+                break;
+        }
+        return true;
+    }
+
+
+    private ConstraintLayout createCounter(){
+        ConstraintLayout counter = (ConstraintLayout) inflater.inflate(R.layout.counter_template_layout, null);
+        DataPicker dp1 = counter.findViewById(R.id.dp1);
+        DataPicker dp10 = counter.findViewById(R.id.dp10);
+        DataPicker dp100 = counter.findViewById(R.id.dp100);
+        DataPicker dp1000 = counter.findViewById(R.id.dp1000);
+        DataPicker dp10000 = counter.findViewById(R.id.dp10000);
+
+        String[] months=new String[10];
+        String[] months1=new String[10];
+        String[] months2=new String[10];
+        String[] months3=new String[10];
+        String[] months4=new String[10];
+
+
+        months[0]="0";
+        months[1]="1";
+        months[2]="2";
+        months[3]="3";
+        months[4]="4";
+        months[5]="5";
+        months[6]="6";
+        months[7]="7";
+        months[8]="8";
+        months[9]="9";
+
+        months1[0]="0";
+        months1[1]="1";
+        months1[2]="2";
+        months1[3]="3";
+        months1[4]="4";
+        months1[5]="5";
+        months1[6]="6";
+        months1[7]="7";
+        months1[8]="8";
+        months1[9]="9";
+
+
+        months2[0]="0";
+        months2[1]="1";
+        months2[2]="2";
+        months2[3]="3";
+        months2[4]="4";
+        months2[5]="5";
+        months2[6]="6";
+        months2[7]="7";
+        months2[8]="8";
+        months2[9]="9";
+
+
+        months3[0]="0";
+        months3[1]="1";
+        months3[2]="2";
+        months3[3]="3";
+        months3[4]="4";
+        months3[5]="5";
+        months3[6]="6";
+        months3[7]="7";
+        months3[8]="8";
+        months3[9]="9";
+
+        months4[0]="0";
+        months4[1]="1";
+        months4[2]="2";
+        months4[3]="3";
+        months4[4]="4";
+        months4[5]="5";
+        months4[6]="6";
+        months4[7]="7";
+        months4[8]="8";
+        months4[9]="9";
+
+
+        dp1.setValues(months);
+        dp10.setValues(months1);
+        dp100.setValues(months2);
+        dp1000.setValues(months3);
+        dp10000.setValues(months4);
+        return counter;
+    }
 }
---
+++ app/src/main/java/ru/startandroid/sendmeters/ru/startandroid/sendmeters/datapicker/DataPicker.java
@@ -0,0 +1,398 @@
+package ru.startandroid.sendmeters.ru.startandroid.sendmeters.datapicker;
+
+import android.content.Context;
+import android.graphics.*;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import ru.startandroid.sendmeters.R;
+
+/**
+ * Created by dev on 27.03.2015.
+ */
+public class DataPicker extends View {
+    public Context dataPickercontext = null;
+    private OnChangeValueListener mListener = null;
+    
+    public int nowTopPosition = 0; //Current scroll position
+    private int upMaxTopPosition = 0; //Max up scroll position
+    private int maxTopPosition = 0; //Max scroll position
+    private int maxValueHeight = 0; //Max of value height
+    private ArrayList<dpValuesSize> dpvalues = new ArrayList<dpValuesSize>(); //Values
+    private int canvasW = 0; //Current weight of canvas
+    private int canvasH = 0; //Current height of canvas
+
+    private int selectedvalueId = 0; //Selected value id
+    private boolean needAnimation = false; //animation
+    private int needPosition = 0; //need to scroll position
+
+    public int valpadding = 30; //vertical values padding
+
+    private int scrollspeed = 0; //Impulse speed of scroll
+    private boolean scrolltoup = false; //The direction of movement
+
+    private float dpDownY = 0; //Touch down coordinates
+    private float canvasDownY = 0; //canvas touch down coordinates
+
+    private long actdownTime = 0; //Touch time
+
+
+    public interface OnChangeValueListener {
+        public void onEvent(int valueId);
+    }
+
+    public void setOnChangeValueListener(OnChangeValueListener eventListener) {
+        mListener = eventListener;
+    }
+    
+    public DataPicker(Context context) {
+        super(context);
+        dataPickercontext = context;
+         }
+    
+    public DataPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        dataPickercontext = context;
+
+    }
+
+    public DataPicker(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        dataPickercontext = context;
+
+    }
+    
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        canvasW = w;
+        canvasH = h;
+        maxValueHeight = (canvasH - (valpadding * 2)) / 2;
+        nowTopPosition = 0;
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            canvasDownY = motionEvent.getY();
+            dpDownY = motionEvent.getY() - nowTopPosition;
+            needAnimation = false;
+            actdownTime = motionEvent.getEventTime();
+        }
+        if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+            if ((int) (motionEvent.getY() - dpDownY) > maxTopPosition) {
+                nowTopPosition = maxTopPosition;
+                return true;
+            }
+            if ((int) (motionEvent.getY() - dpDownY) < upMaxTopPosition) {
+                nowTopPosition = upMaxTopPosition;
+                return true;
+            }
+            nowTopPosition = (int) (motionEvent.getY() - dpDownY);
+        }
+
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            if (canvasDownY > motionEvent.getY()) {
+                scrolltoup = false;
+            } else {
+                scrolltoup = true;
+            }
+
+            if ((motionEvent.getEventTime() - actdownTime < 200) && (Math.abs(dpDownY - motionEvent.getY()) > 100)) {
+                scrollspeed = (int) (1000 - (motionEvent.getEventTime() - actdownTime));
+            } else {
+                scrollspeed = 0;
+                roundingValue();
+            }
+            needAnimation = true;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        try {
+            if (dpvalues.size() == 0) {
+                return;
+            }
+
+            upMaxTopPosition = -(((dpvalues.size() - 1) * (maxValueHeight + valpadding)));
+            canvas.drawColor(Color.argb(0, 255, 255, 255));
+
+            if (needAnimation) {
+                if (scrollspeed > 0) {
+                    scrollspeed -= 30;
+                    if (scrolltoup) {
+                        int currentPos = nowTopPosition + 30;
+                        if ((currentPos) > maxTopPosition) {
+                            nowTopPosition = maxTopPosition;
+                            scrollspeed = 0;
+                            roundingValue();
+                        } else {
+                            nowTopPosition = currentPos;
+                        }
+                    }
+
+                    if (!scrolltoup) {
+                        int currentPos = nowTopPosition - 30;
+                        if ((currentPos) < upMaxTopPosition) {
+                            nowTopPosition = upMaxTopPosition;
+                            scrollspeed = 0;
+                            roundingValue();
+                        } else {
+                            nowTopPosition = currentPos;
+                        }
+                    }
+
+                    if (scrollspeed <= 0) {
+                        roundingValue();
+                    }
+                } else {
+                    if (nowTopPosition > needPosition) {
+                        nowTopPosition -= 20;
+                        if (nowTopPosition < needPosition) {
+                            nowTopPosition = needPosition;
+                        }
+                    }
+
+                    if (nowTopPosition < needPosition) {
+                        nowTopPosition += 20;
+                        if (nowTopPosition > needPosition) {
+                            nowTopPosition = needPosition;
+                        }
+                    }
+
+                    if (nowTopPosition == needPosition) {
+                        needAnimation = false;
+                    }
+                }
+            }
+
+            //Paste values to canvas
+            for (int i = 0; i < dpvalues.size(); i++) {
+                try {
+                    Paint paint = new Paint();
+                    paint.setColor(dataPickercontext.getResources().getColor(R.color.datepickerText));
+                    if (selectedvalueId == i) {
+                        paint.setColor(dataPickercontext.getResources().getColor(R.color.datepickerSelectedValue));
+                        Paint shadowText = new Paint();
+                        shadowText.setColor(dataPickercontext.getResources().getColor(R.color.datepickerSelectedValueShadow));
+                        shadowText.setTextSize(dpvalues.get(i).dpTextSize);
+                        shadowText.setAntiAlias(true);
+                        canvas.drawText(dpvalues.get(i).dpValue, (canvasW / 2) - (dpvalues.get(i).dpWidth / 2), ((maxValueHeight + valpadding) * i) + (valpadding + maxValueHeight) + (dpvalues.get(i).dpHeight / 2) + nowTopPosition + 2, shadowText);
+                    }
+                    paint.setTextSize(dpvalues.get(i).dpTextSize);
+                    paint.setAntiAlias(true);
+                    canvas.drawText(dpvalues.get(i).dpValue, (canvasW / 2) - (dpvalues.get(i).dpWidth / 2), ((maxValueHeight + valpadding) * i) + (valpadding + maxValueHeight) + (dpvalues.get(i).dpHeight / 2) + nowTopPosition, paint);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Draw borders
+            Paint lPBorders = new Paint();
+            lPBorders.setColor(dataPickercontext.getResources().getColor(R.color.datapickerBlackLines));
+            canvas.drawLine(0, 0, 0, canvasH, lPBorders);
+            canvas.drawLine(1, 0, 1, canvasH, lPBorders);
+            canvas.drawLine(canvasW - 1, 0, canvasW - 1, canvasH, lPBorders);
+            canvas.drawLine(canvasW - 2, 0, canvasW - 2, canvasH, lPBorders);
+            canvas.drawLine(canvasW, 0, canvasW, canvasH, lPBorders);
+            
+            lPBorders = new Paint();
+            lPBorders.setColor(dataPickercontext.getResources().getColor(R.color.datapickerGrayLines));
+            canvas.drawRect(2, 0, 7, canvasH, lPBorders);
+            canvas.drawRect(canvasW - 7, 0, canvasW - 2, canvasH, lPBorders);
+
+            //Draw shadows
+            Paint framePaint = new Paint();
+            framePaint.setShader(new LinearGradient(0, 0, 0, getHeight() / 5, dataPickercontext.getResources().getColor(R.color.datapickerGradientStart), Color.TRANSPARENT, Shader.TileMode.CLAMP));
+            canvas.drawPaint(framePaint);
+            framePaint.setShader(new LinearGradient(0, getHeight(), 0, getHeight() - getHeight() / 5, dataPickercontext.getResources().getColor(R.color.datapickerGradientStart), Color.TRANSPARENT, Shader.TileMode.CLAMP));
+            canvas.drawPaint(framePaint);
+
+            //Draw glass
+            Path pathSelect = new Path();
+            pathSelect.moveTo(0, canvasH / 2 - maxValueHeight / 2 - valpadding / 2);
+            pathSelect.lineTo(canvasW, canvasH / 2 - maxValueHeight / 2 - valpadding / 2);
+            pathSelect.lineTo(canvasW, canvasH / 2);
+            pathSelect.lineTo(0, canvasH / 2);
+            pathSelect.lineTo(0, canvasH / 2 - maxValueHeight / 2);
+            Paint pathSelectPaint = new Paint();
+            pathSelectPaint.setShader(new LinearGradient(0, 0, 0, maxValueHeight / 2, dataPickercontext.getResources().getColor(R.color.datapickerSelectedValueeLineG1), dataPickercontext.getResources().getColor(R.color.datapickerSelectedValueeLineG2), Shader.TileMode.CLAMP));
+            canvas.drawPath(pathSelect, pathSelectPaint);
+
+            pathSelect = new Path();
+            pathSelect.moveTo(0, canvasH / 2);
+            pathSelect.lineTo(canvasW, canvasH / 2);
+            pathSelect.lineTo(canvasW, canvasH / 2 + maxValueHeight / 2 + valpadding / 2);
+            pathSelect.lineTo(0, canvasH / 2 + maxValueHeight / 2 + valpadding / 2);
+            pathSelect.lineTo(0, canvasH / 2);
+            pathSelectPaint = new Paint();
+            pathSelectPaint.setShader(new LinearGradient(0, 0, 0, maxValueHeight / 2, dataPickercontext.getResources().getColor(R.color.datapickerSelectedValueeLineG3), dataPickercontext.getResources().getColor(R.color.datapickerSelectedValueeLineG4), Shader.TileMode.CLAMP));
+            canvas.drawPath(pathSelect, pathSelectPaint);
+
+            //Draw glass shadow
+            Paint selValLightBorder = new Paint();
+            Paint selValTopBorder = new Paint();
+            Paint selValBottomBorder = new Paint();
+            selValLightBorder.setColor(dataPickercontext.getResources().getColor(R.color.datapicketSelectedValueBorder));
+            selValTopBorder.setColor(dataPickercontext.getResources().getColor(R.color.datapicketSelectedBorderTop));
+            selValBottomBorder.setColor(dataPickercontext.getResources().getColor(R.color.datapicketSelectedBorderBttom));
+            canvas.drawLine(0, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, canvasW, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, selValLightBorder);
+            canvas.drawLine(0, canvasH / 2 - maxValueHeight / 2 - valpadding / 2 + 1, canvasW, canvasH / 2 - maxValueHeight / 2 - valpadding / 2 + 1, selValTopBorder);
+            canvas.drawLine(0, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, canvasW, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, selValLightBorder);
+            canvas.drawLine(0, canvasH / 2 + maxValueHeight / 2 + valpadding / 2 - 1, canvasW, canvasH / 2 + maxValueHeight / 2 + valpadding / 2 - 1, selValBottomBorder);
+            canvas.drawLine(0, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, 0, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, selValLightBorder);
+            canvas.drawLine(1, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, 1, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, selValLightBorder);
+            canvas.drawLine(canvasW - 1, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, canvasW - 1, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, selValLightBorder);
+            canvas.drawLine(canvasW - 2, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, canvasW - 2, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, selValLightBorder);
+            canvas.drawLine(canvasW, canvasH / 2 - maxValueHeight / 2 - valpadding / 2, canvasW, canvasH / 2 + maxValueHeight / 2 + valpadding / 2, selValLightBorder);
+
+            //Draw selected value
+            Paint selectedTextPaint = new Paint();
+            selectedTextPaint.setColor(dataPickercontext.getResources().getColor(R.color.datepickerSelectedValue));
+            Paint shadowText = new Paint();
+            shadowText.setColor(dataPickercontext.getResources().getColor(R.color.datepickerSelectedValueShadow));
+            shadowText.setTextSize(dpvalues.get(selectedvalueId).dpTextSize);
+            shadowText.setAntiAlias(true);
+            canvas.drawText(dpvalues.get(selectedvalueId).dpValue, (canvasW / 2) - (dpvalues.get(selectedvalueId).dpWidth / 2), ((maxValueHeight + valpadding) * selectedvalueId) + (valpadding + maxValueHeight) + (dpvalues.get(selectedvalueId).dpHeight / 2) + nowTopPosition + 2, shadowText);
+            selectedTextPaint.setTextSize(dpvalues.get(selectedvalueId).dpTextSize);
+            selectedTextPaint.setAntiAlias(true);
+            canvas.drawText(dpvalues.get(selectedvalueId).dpValue, (canvasW / 2) - (dpvalues.get(selectedvalueId).dpWidth / 2), ((maxValueHeight + valpadding) * selectedvalueId) + (valpadding + maxValueHeight) + (dpvalues.get(selectedvalueId).dpHeight / 2) + nowTopPosition, selectedTextPaint);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //FPS to canvas
+        this.postInvalidateDelayed(1000 / 50);
+    }
+
+
+    //Rounding values
+    private void roundingValue() {
+        Log.d("Rounding", "ROUNDING VALUE!");
+        needPosition = (((nowTopPosition - maxTopPosition - (maxValueHeight / 2)) / (maxValueHeight + valpadding))) * (maxValueHeight + valpadding) + maxTopPosition;
+        selectedvalueId = Math.abs(((needPosition - valpadding - (maxValueHeight / 2)) / (maxValueHeight + valpadding)));
+        try {
+            Log.e("SELECTED VALUE", dpvalues.get(selectedvalueId).dpValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        onSelected(selectedvalueId);
+    }
+
+    //Return value
+    public String getValue() {
+        try {
+            return dpvalues.get(selectedvalueId).dpValue;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //Set values
+    private Handler dpChangeHandler = new Handler();
+
+    public void changetToValue(final int valueId) {
+        selectedvalueId = valueId;
+
+        dpChangeHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (canvasW == 0 || canvasH == 0) {
+                    dpChangeHandler.postDelayed(this, 100);
+                } else {
+                    maxValueHeight = (canvasH - (valpadding * 2)) / 2;
+                    needPosition = -((maxValueHeight + valpadding) * (valueId));
+                    needAnimation = true;
+                }
+            }
+        }, 100);
+
+        onSelected(selectedvalueId);
+    }
+
+
+    //On value selected Event
+    protected void onSelected(int selectedId) {
+        if (mListener != null) {
+            mListener.onEvent(selectedId);
+        }
+    }
+
+
+    //Return the value id
+    public int getValueid() {
+        try {
+            return selectedvalueId;
+        } catch (Exception e) {
+        }
+        return -1;
+    }
+
+
+    private Handler dpHandler = new Handler();
+    //Set values
+    public void setValues(final String[] newvalues) {
+        if (canvasW == 0 || canvasH == 0) {
+            dpHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (canvasW == 0 || canvasH == 0) {
+                        dpHandler.postDelayed(this, 100);
+                    } else {
+                        dpvalues.clear();
+                        for (int i = 0; i < newvalues.length; i++) {
+                            dpvalues.add(new dpValuesSize(newvalues[i], canvasW, canvasH));
+                        }
+                    }
+                }
+            }, 100);
+        }
+        dpvalues.clear();
+        for (int i = 0; i < newvalues.length; i++) {
+            dpvalues.add(new dpValuesSize(newvalues[i], canvasW, canvasH));
+        }
+    }
+}
+
+
+class dpValuesSize {
+    public int dpWidth = 0;
+    public int dpHeight = 0;
+    public String dpValue = "";
+    public int dpTextSize = 0;
+    public int valpadding = 30;
+    public int valinnerLeftpadding = 20;
+
+    public dpValuesSize(String val, int canvasW, int canvasH) {
+        try {
+            int maxTextHeight = (canvasH - (valpadding * 2)) / 2;
+            boolean sizeOK = false;
+            dpValue = val;
+            while (!sizeOK) {
+                Rect textBounds = new Rect();
+                Paint textPaint = new Paint();
+                dpTextSize++;
+                textPaint.setTextSize(dpTextSize);
+                textPaint.getTextBounds(val, 0, val.length(), textBounds);
+                if (textBounds.width() <= canvasW - (valinnerLeftpadding * 2) && textBounds.height() <= maxTextHeight) {
+                    dpWidth = textBounds.width();
+                    dpHeight = textBounds.height();
+                } else {
+                    sizeOK = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
