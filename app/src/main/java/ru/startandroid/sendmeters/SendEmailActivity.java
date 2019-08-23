6dd8cfdf33aabe3b5aad574edb1ece96e520072e
Denis Polulyakh <polulyakhdm@it2g.ru>
2019-08-23 17:34:51 +0400

    add counter layout

--- app/src/main/java/ru/startandroid/sendmeters/SendEmailActivity.java
+++ app/src/main/java/ru/startandroid/sendmeters/SendEmailActivity.java
@@ -1,9 +1,11 @@
 package ru.startandroid.sendmeters;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -12,12 +14,16 @@ import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import ru.startandroid.sendmeters.model.Counter;
 import ru.startandroid.sendmeters.ru.startandroid.sendmeters.datapicker.DataPicker;
 
 public class SendEmailActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
@@ -28,7 +34,7 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
     private EditText etElectricityMeter;
     private Button btnAddCounter;
 
-    private List<ConstraintLayout> counters;
+    private List<Counter> counters;
     private float fromPosition;
     private ViewFlipper flipper;
     private LayoutInflater inflater;
@@ -40,8 +46,7 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnAddCounter = (Button) findViewById(R.id.btnAddCounter);
-        btnAddCounter.setOnClickListener(this);
+
         counters = new ArrayList<>();
         // Устанавливаем listener касаний, для последующего перехвата жестов
         ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.mainLayout);
@@ -52,19 +57,23 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
 
         // Создаем View и добавляем их в уже готовый flipper
         inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //flipper.addView(createCounter());
-        //flipper.addView(createCounter());
-        //flipper.showNext();
 
+        ConstraintLayout addLayout = (ConstraintLayout) inflater.inflate(R.layout.add_layout, null);
+        btnAddCounter = (Button) addLayout.findViewById(R.id.btnAddCounter);
+        btnAddCounter.setOnClickListener(this);
+        flipper.addView(addLayout);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnAddCounter:
-                ConstraintLayout counter = createCounter();
-                counters.add(counter);
-                flipper.addView(counter);
+
+                createCounterDialog(flipper, counters);
+
+
+               // counters.add(counterData);
+               // flipper.addView(counterLayout);
 
         }
     }
@@ -99,7 +108,7 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
         String electricity = etElectricityMeter.getText().toString();
 
         String patternEmail = getResources().getString(R.string.EmailText);
-        patternEmail = patternEmail.replace("{month}",month);
+        patternEmail = patternEmail.replace("{month}", month);
         patternEmail = patternEmail.replace("{KithenColdWaterNumber}", getResources().getString(R.string.KithenColdWaterNumber));
         patternEmail = patternEmail.replace("{BathRoomColdWaterNumber}", getResources().getString(R.string.BathRoomColdWaterNumber));
         patternEmail = patternEmail.replace("{KithenHotWaterNumber}", getResources().getString(R.string.KithenHotWaterNumber));
@@ -127,12 +136,15 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
                 if (fromPosition > toPosition) {
                     flipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.go_next_in));
                     flipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.go_next_out));
-                    flipper.showNext();
+                    if (counters.size() > 0) {
+                        flipper.showNext();
+                    }
                 } else if (fromPosition < toPosition) {
                     flipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.go_prev_in));
                     flipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.go_prev_out));
-                    flipper.showPrevious();
-                    flipper.getCurrentView()
+                    if (counters.size() > 0) {
+                        flipper.showPrevious();
+                    }
                 }
             default:
                 break;
@@ -141,84 +153,92 @@ public class SendEmailActivity extends AppCompatActivity implements View.OnClick
     }
 
 
-    private ConstraintLayout createCounter(){
+    private ConstraintLayout createCounter(Counter counterData) {
         ConstraintLayout counter = (ConstraintLayout) inflater.inflate(R.layout.counter_template_layout, null);
+        TextView tvRoom = counter.findViewById(R.id.tvNameRoom);
+        TextView tvDescriptionCount = counter.findViewById(R.id.tvDescriptionCount);
+        TextView tvNumberCount = counter.findViewById(R.id.tvNumberCount);
+
+        tvRoom.setText(counterData.getRoom());
+        tvDescriptionCount.setText(counterData.getDescription());
+        tvNumberCount.setText(counterData.getNumber());
+
         DataPicker dp1 = counter.findViewById(R.id.dp1);
         DataPicker dp10 = counter.findViewById(R.id.dp10);
         DataPicker dp100 = counter.findViewById(R.id.dp100);
         DataPicker dp1000 = counter.findViewById(R.id.dp1000);
         DataPicker dp10000 = counter.findViewById(R.id.dp10000);
+        String[] numbersCount;
+        for (int i = 0; i < 5; i++) {
+            numbersCount = new String[10];
+            for (int j = 0; j < 10; j++) {
+                numbersCount[j] = "" + j;
+            }
+            switch (i) {
+                case 0:
+                    dp1.setValues(numbersCount);
+                    break;
+                case 1:
+                    dp10.setValues(numbersCount);
+                    break;
+                case 2:
+                    dp100.setValues(numbersCount);
+                    break;
+                case 3:
+                    dp1000.setValues(numbersCount);
+                    break;
+                case 4:
+                    dp10000.setValues(numbersCount);
+                    break;
+            }
 
-        String[] months=new String[10];
-        String[] months1=new String[10];
-        String[] months2=new String[10];
-        String[] months3=new String[10];
-        String[] months4=new String[10];
-
-
-        months[0]="0";
-        months[1]="1";
-        months[2]="2";
-        months[3]="3";
-        months[4]="4";
-        months[5]="5";
-        months[6]="6";
-        months[7]="7";
-        months[8]="8";
-        months[9]="9";
-
-        months1[0]="0";
-        months1[1]="1";
-        months1[2]="2";
-        months1[3]="3";
-        months1[4]="4";
-        months1[5]="5";
-        months1[6]="6";
-        months1[7]="7";
-        months1[8]="8";
-        months1[9]="9";
-
-
-        months2[0]="0";
-        months2[1]="1";
-        months2[2]="2";
-        months2[3]="3";
-        months2[4]="4";
-        months2[5]="5";
-        months2[6]="6";
-        months2[7]="7";
-        months2[8]="8";
-        months2[9]="9";
-
-
-        months3[0]="0";
-        months3[1]="1";
-        months3[2]="2";
-        months3[3]="3";
-        months3[4]="4";
-        months3[5]="5";
-        months3[6]="6";
-        months3[7]="7";
-        months3[8]="8";
-        months3[9]="9";
-
-        months4[0]="0";
-        months4[1]="1";
-        months4[2]="2";
-        months4[3]="3";
-        months4[4]="4";
-        months4[5]="5";
-        months4[6]="6";
-        months4[7]="7";
-        months4[8]="8";
-        months4[9]="9";
-
-
-        dp1.setValues(months);
-        dp10.setValues(months1);
-        dp100.setValues(months2);
-        dp1000.setValues(months3);
-        dp10000.setValues(months4);
+        }
         return counter;
+    }
+
+
+    private void createCounterDialog(final ViewFlipper flipper, final List<Counter> counters) {
+        ConstraintLayout editCounter = (ConstraintLayout) inflater.inflate(R.layout.counter_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+        alertDialogBuilder.setView(editCounter);
+        final EditText room = (EditText) editCounter.findViewById(R.id.etRoom);
+        final EditText description = (EditText) editCounter.findViewById(R.id.etDescr);
+        final EditText number = (EditText) editCounter.findViewById(R.id.etNumber);
+        final Counter counter = new Counter();
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Добавить счетчик",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                counter.setDescription(description.getText().toString());
+                                counter.setRoom(room.getText().toString());
+                                counter.setNumber(number.getText().toString());
+                            }
+                        })
+                .setNegativeButton("Отмена",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        ConstraintLayout counterLayout = createCounter(counter);
+                        counter.setView(counterLayout);
+                        counters.add(counter);
+                        flipper.addView(counterLayout);
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+
     }
 }
---
+++ app/src/main/java/ru/startandroid/sendmeters/model/Counter.java
@@ -0,0 +1,22 @@
+package ru.startandroid.sendmeters.model;
+
+import android.view.View;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Counter {
+    private String room;
+    private String description;
+    private String number;
+    private String meter;
+    private View view;
+}
